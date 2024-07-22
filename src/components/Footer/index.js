6f5c58af8636fdf866a35add070e8cddc39f03d4
MillerFelix <millerredigolo@gmail.com>
2024-07-22 14:28:36 -0300

    Uploading several creations developed during the course in this first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h2>Movies &copy; Desenvolvido por Miller Felix 2023</h2>
+    </footer>
+  );
+}
+
+export default Footer;
