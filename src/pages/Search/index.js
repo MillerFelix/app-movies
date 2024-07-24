fc17ac07103065de92f815f07ef3828acc92fcc8
MillerFelix <millerredigolo@gmail.com>
2024-07-24 09:31:01 -0300

    Developing video search functionality

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,7 +1,7 @@
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import VideoList from "../../components/VideoList";
+import SearchVideoList from "../../components/SearchVideoList";
 import videos from "../../json/videos.json";
 
 import styles from "./Search.module.css";
@@ -13,7 +13,7 @@ function Search() {
       <Container>
         <section className={styles.search}>
           <h2>Pesquisar</h2>
-          <VideoList videos={videos} />
+          <SearchVideoList videos={videos} />
         </section>
       </Container>
       <Footer />
