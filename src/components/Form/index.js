ecef4bea75465a94f32e1a56f93936217588f641
MillerFelix <millerredigolo@gmail.com>
2024-07-26 09:17:52 -0300

    Developing Validation for Form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,22 +1,70 @@
+import { useState } from "react";
 import { categories } from "../Category";
 import styles from "./Form.module.css";
 
 function Form() {
+  const [url, setUrl] = useState("");
+  const [category, setCategory] = useState("");
+  const [videos, setVideos] = useState([]);
+  const [errors, setErrors] = useState("");
+
+  const valideUrl = (url) => {
+    const regex =
+      /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=)|youtu\.be\/)([a-zA-Z0-9\-_]+)$/;
+
+    if (!regex.test(url) || url.length < 43) {
+      setErrors("ERRO: URL inválida...");
+      return false;
+    } else {
+      return url.substring(32, 43);
+    }
+  };
+
+  const onSave = (e) => {
+    e.preventDefault();
+
+    if (!category || category === "-") {
+      setErrors("ERRO: Escolha uma categoria...");
+      return;
+    } else {
+      setErrors("");
+    }
+
+    const urlVideo = valideUrl(url);
+    if (urlVideo && category) {
+      const newVideo = { url, category };
+      setVideos([...videos, newVideo]);
+      localStorage.setItem("videos", JSON.stringify([...videos, newVideo]));
+      setUrl("");
+      setVideos("");
+    } else {
+      setErrors("ERRO: URL inválida");
+    }
+  };
+
   return (
     <section className={styles.container}>
       <h2>Cadastro de vídeo</h2>
-      <form>
+      <form onSubmit={onSave}>
         <div>
           <label>URL do vídeo</label>
           <input
             type="text"
             placeholder="Digite a URL do vídeo"
             required="required"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            maxLength="43"
+            minLength="43"
           ></input>
         </div>
         <div>
           <label>Categoria</label>
-          <select>
+          <select
+            required="required"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="-">Selecione uma categoria</option>
             {categories.map((category) => {
               return <option value={category}>{category}</option>;
@@ -24,8 +72,9 @@ function Form() {
           </select>
         </div>
         <div>
-          <button>Cadastrar</button>
+          <button type="submit">Cadastrar</button>
         </div>
+        <div>{errors}</div>
       </form>
     </section>
   );
